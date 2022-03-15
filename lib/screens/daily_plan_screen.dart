e6a1e545f25e198de0af571cca707f882d2cf17c
rustam.abdiraxmanov <telegram707707@gmail.com>
2022-03-15 17:34:52 +0500

    to do app

--- lib/screens/daily_plan_screen.dart
+++ lib/screens/daily_plan_screen.dart
@@ -1,5 +1,4 @@
 import 'package:dotted_border/dotted_border.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
